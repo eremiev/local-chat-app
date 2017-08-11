60c4ad32734c02437418f11233a4336b445941a4
eremiev <emil.eremiev@abv.bg>
2017-08-11 09:22:30 +0300

    send messages to different clients

--- server.js
+++ server.js
@@ -7,9 +7,19 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+var clients = 0;
+
 //Whenever someone connects this gets executed
 io.on('connection', function(socket){
   console.log('A user connected');
+
+  clients++;
+  //Send message to all.
+  io.sockets.emit('chat message', clients + ' clients connected!');
+  //Send message to current connected client.
+  socket.emit('chat message','Hey, welcome!');
+  //Send message to other clients.
+  socket.broadcast.emit('chat message', 'One client is joining.')
 
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
@@ -19,16 +29,12 @@ io.on('connection', function(socket){
   //Whenever someone disconnects this piece of code executed
   socket.on('disconnect', function () {
     console.log('A user disconnected');
+    clients--;
   });
 
 });
 
-
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
-
-
 
